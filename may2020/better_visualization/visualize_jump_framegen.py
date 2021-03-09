cd6c97676d5a179aec0981f10ff50c20c2ffa6c8
Serena Sian Yuan <ssy248@nyu.edu>
2021-03-08 21:36:49 -0800

    Update visualize_jump_framegen.py

--- may2020/better_visualization/visualize_jump_framegen.py
+++ may2020/better_visualization/visualize_jump_framegen.py
@@ -1,5 +1,3 @@
-# jump ahead to future next frame gen
-
 # reset frame gen
 frame_gen = FrameGen(pcap_file_path,detecting_range,bck_voxel_path,with_bf=False).DBSCAN_pcap_frame_generator(eps,min_samples)
 
@@ -10,19 +8,19 @@ frame_gen = FrameGen(pcap_file_path,detecting_range,bck_voxel_path,with_bf=False
 
 fgs = []
 
-totlen = 40
+totlen = 20
 
 # start from future frame
-future= 10 
+future= 20 
 
-nframe_gen = frame_gen
 for i in range(0, future):
-    nframe_gen = next(nframe_gen)
+    nframe_gen = next(frame_gen)
 
+print("length of starting frame is", len(nframe_gen))
+    
 for i in range(0, totlen):
     fgi = frequency_grid()
     fgi.setup_grid()
-    
     fgi.set_first_coordinates(nframe_gen, i)
     fgs.append(fgi)
 
@@ -34,9 +32,10 @@ for i in range(0, totlen):
     # reset frame gen 
     # reset frame gen
     frame_gen = FrameGen(pcap_file_path,detecting_range,bck_voxel_path,with_bf=False).DBSCAN_pcap_frame_generator(eps,min_samples)
-    nframe_gen = frame_gen
-    for i in range(0, future):
-        nframe_gen = next(nframe_gen)
+    
+    for j in range(0, future):
+        nframe_gen =next(frame_gen)
+        
     t = True
     fg1 = fgs[i]
     while t:
